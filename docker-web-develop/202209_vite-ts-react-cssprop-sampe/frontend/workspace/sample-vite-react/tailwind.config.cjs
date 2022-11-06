3206c3a42ae7c282200fe6e172c23c44c40bac93
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-11-06 23:46:07 +0900

    tailwindcssの設定変更

--- docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/tailwind.config.cjs
+++ docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/tailwind.config.cjs
@@ -5,6 +5,20 @@ module.exports = {
     "src/**/*{jsx,tsx}"
   ],
   theme: {
+    screens: {
+      /*
+      - Rules
+        - 「～n」はminのみ，「～x」はmaxのみ．
+      - Refs
+        - https://tailwindcss.com/docs/screens
+      */
+      "smn": "640px", // => @media (min-width: 640px) { ... }
+      "mdn": "768px", // => @media (min-width: 768px) { ... }
+      "mdx": {"max": "768px"}, // => @media (max-width: 768px) { ... }
+      "lgn": "1024px", // => @media (min-width: 1024px) { ... }
+      "xln": "1280px", // => @media (min-width: 1280px) { ... }
+      "2xln": "1536px", // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
