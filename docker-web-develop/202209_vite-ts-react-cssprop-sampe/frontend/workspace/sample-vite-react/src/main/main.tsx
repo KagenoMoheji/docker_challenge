bea32939e7ec17a02acd6661953cf682d3d5e95d
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-11-06 23:48:01 +0900

    ReactRouterの導入に向けたコンポーネントの実装場所変更に合わせた変更．ApolloClientのサンプルコンポーネントや関連コードがあるがこれは後で有効化する．

--- docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/main.tsx
+++ docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "~/index.scss"; // cssからscssに変更した
-import {Root} from "~/routes"; // ReactRouterDOMの実装時に追加
+import "~/styles/base.scss";
+
+import {App} from "~/routes";
+// import {ApolloProvider} from "@apollo/client";
+// import {client} from "~/plugins/@apollo/client/ApolloClient";
+// import {worker} from "~~/mock_graphql/msw-graphql/browser";
 
 /*
 [我流実装ルール的な]
@@ -10,9 +14,19 @@ import {Root} from "~/routes"; // ReactRouterDOMの実装時に追加
 - 各Reactコンポーネントのトップのクラス名に`comp-{コンポーネント名}`または`tmpl-{テンプレート名}`を記述
 */
 
+// if(process.env.NODE_ENV === "development"){
+//   // 開発モードの場合にMSWを有効化
+//   await worker.start({
+//     serviceWorker:{
+//       url: "/mockServiceWorker.js"
+//     }
+//   });
+// }
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Root />
+    {/* <ApolloProvider client={client}> */}
+    <App />
+    {/* </ApolloProvider> */}
   </React.StrictMode>
 );
