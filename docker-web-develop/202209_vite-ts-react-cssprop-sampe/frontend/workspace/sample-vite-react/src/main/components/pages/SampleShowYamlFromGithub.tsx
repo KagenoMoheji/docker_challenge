906737a0de4975fc6bdf19da672550f0c94b0e68
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-11-10 01:30:50 +0900

    useMemoサンプル追加

--- docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/components/pages/SampleShowYamlFromGithub.tsx
+++ docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/components/pages/SampleShowYamlFromGithub.tsx
@@ -5,7 +5,7 @@
   - https://maku77.github.io/nodejs/io/yaml.html
 */
 import yaml from "js-yaml";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useMemo} from "react";
 import axios from "axios";
 import {useQueryStrings} from "~/utils/reacthooks/react-router-dom";
 
@@ -26,7 +26,17 @@ type Article = {
 export const SampleShowYamlFromGithub = (): JSX.Element => {
   const [ymlArticle, setYmlArticle] = useState<string>("");
   const qstrs = useQueryStrings();
-  useEffect(() => {
+  // useEffect(() => {
+  //   axios.get("https://api.github.com/repos/KagenoMoheji/test/contents/hoge.yml", {})
+  //     .then((res) => {
+  //       setYmlArticle(decodeURIComponent(escape(atob(res.data.content))));
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+  // MEMO: useEffectよりuseMemoを使った方が処理回数を抑えられる？？
+  useMemo(() => {
     axios.get("https://api.github.com/repos/KagenoMoheji/test/contents/hoge.yml", {})
       .then((res) => {
         setYmlArticle(decodeURIComponent(escape(atob(res.data.content))));
