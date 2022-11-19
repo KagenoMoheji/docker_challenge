97bb29779ed173fbc58a784a84b4e61649e5915e
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-11-20 03:45:21 +0900

    useSelectorはコンポーネント外で書いたほうがいいみたいなので修正

--- docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/components/pages/SampleReduxToolkit.tsx
+++ docker-web-develop/202209_vite-ts-react-cssprop-sampe/frontend/workspace/sample-vite-react/src/main/components/pages/SampleReduxToolkit.tsx
@@ -3,10 +3,11 @@ import {sampleSelectors, sampleActions} from "~/store/sample";
 
 export const SampleReduxToolkit = (): JSX.Element => {
   const dispatch = useDispatch();
+  const count = useSelector(sampleSelectors.selectCount());
   return (
     <div className="comp-samplereduxtoolkit">
       <button onClick={() => dispatch(sampleActions.increment({diff: 1}))}>＋</button>
-      <span>{useSelector(sampleSelectors.selectCount())}</span>
+      <span>{count}</span>
       <button onClick={() => dispatch(sampleActions.decrement({diff: 1}))}>－</button>
     </div>
   );
